a0dcdfe61ce45f507ad98fee11934cf5e9ecf3f1
Ian Goodnight <goodnight.ian@gmail.com>
2020-10-27 13:07:10 -0400

    added SV configuration added author attribution

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 4000;
-
+const config = require('./configuration.js');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res, next) {
 	res.render('index', { title: 'ohmigosh' });
 });
+console.log(config.SV_TENANT_TOKEN);
 app.listen(port, () => {
 	console.log(`Skus-me listening at http://localhost:${port}`);
 });
