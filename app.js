1cc3d1c670a0ea62fddc153d24320b60bef53bdf
Ian Goodnight <goodnight.ian@gmail.com>
2020-11-24 12:30:40 -0500

    working on unit testing

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
 const express = require('express');
+
 const app = express();
+
 const path = require('path');
+
 const port = process.env.PORT || 4000;
-const config = require('./configuration.js');
+
 const axios = require('axios');
+
+const config = require('./configuration.js');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function(req, res, next) {
-	res.render('index', { title: 'ohmigosh' });
+app.get('/', (req, res) => {
+  res.render('index', { title: 'ohmigosh' });
 });
-app.post('/api', function(req, res, next) {
-	var target = req.body.target || 'ProductCode';
-	var value = req.body.value || '';
-	axios({
-		method: 'POST',
-		url: 'https://app.skuvault.com/api/products/getProduct',
-		headers: {
-			'Content-Type': 'application/json',
-			'Accept': 'application/json'
-		},
-		data: {
-			[target] : value,
-			TenantToken : config.SV_TENANT_TOKEN,
-			UserToken : config.SV_USER_TOKEN
-		}
-	}).then(response => res.json(response.data))
-	.catch(error => res.json({Errors: [,,,]}));
-})
+app.post('/api', (req, res) => {
+  const target = req.body.target || 'ProductCode';
+  const value = req.body.value || '';
+  axios({
+    method: 'POST',
+    url: 'https://app.skuvault.com/api/products/getProduct',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    data: {
+      [target]: value,
+      TenantToken: config.SV_TENANT_TOKEN,
+      UserToken: config.SV_USER_TOKEN,
+    },
+  }).then((response) => res.json(response.data))
+    .catch(() => res.json({ Errors: ['', '', '', ''] }));
+});
 app.listen(port, () => {
-	console.log(`Skus-me listening at http://localhost:${port}`);
+  console.log(`Skus-me listening at http://localhost:${port}`);
 });
+
+try {
+  module.exports = app;
+} catch (error) {
+  console.log(`Error exporting app: ${error}`);
+}
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,40 +1,46 @@
+/* eslint-env browser */
+
 (function main() {
-	var form = document.querySelector('#input form');
-	form.addEventListener('submit', function(e) {
-		e.preventDefault();
-		var sku = document.getElementById('sku').value;
-		var code = document.getElementById('code').value;
-		var target = sku.length > 4 ? 'ProductSKU' : 'ProductCode';
-		var value = target == 'ProductSKU' ? sku : code;
-		postData(target, value)
-			.then(response => write(response)) 
-			.catch(error => write({ Errors: [,,,] }));
-	});
+  const form = document.querySelector('#input form');
 
-	async function postData(target = 'codes', value = '') {
-		const response = await fetch('/api', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				target: target,
-				value: value
-			})
-		});
-		return response.json();
-	}
+  async function postData(target = 'codes', value = '') {
+    const response = await fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        target,
+        value,
+      }),
+    });
+    return response.json();
+  }
 
-	function write(obj = {}) {
-		var output = '';
-		if (obj.Errors.length >= 1) {
-			document.getElementById('display').innerHTML = '<div>Whoopsie!</div>';
-			return;
-		}
-		for (const [key, value] of Object.entries(obj.Product)) {
-			if (key == "SupplierInfo") continue;
-			output += `<div><span class="key">${key} : </span><span>${value}</span></div>`;
-		}
-		document.getElementById('display').innerHTML = output;
-	}
-})();
+  function write(obj = {}) {
+    let output = '';
+    const products = obj.Product || {};
+    if (obj.Errors.length >= 1) {
+      document.getElementById('display').innerHTML = '<div>Whoopsie!</div>';
+      return;
+    }
+    Object.keys(products).forEach((key) => {
+      if (key !== 'SupplierInfo') {
+        output += `<div><span class="key">${key} : \
+          </span><span>${products[key]}</span></div>`;
+      }
+    });
+    document.getElementById('display').innerHTML = output;
+  }
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const sku = document.getElementById('sku').value;
+    const code = document.getElementById('code').value;
+    const target = sku.length > 4 ? 'ProductSKU' : 'ProductCode';
+    const value = target === 'ProductSKU' ? sku : code;
+    postData(target, value)
+      .then((response) => write(response))
+      .catch(() => write({ Errors: ['', '', '', ''] }));
+  });
+}());
